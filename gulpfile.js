73fa77f41cfc59545ce1e51672968d27b2ffef24
vignatov <vignatov.new@gmail.com>
2018-11-21 23:18:44 +0300

    gulfile task changed

--- gulpfile.js
+++ gulpfile.js
@@ -16,19 +16,6 @@ const sourcemaps = require(`gulp-sourcemaps`);
 const mocha = require(`gulp-mocha`);
 const commonjs = require(`rollup-plugin-commonjs`);
 
-gulp.task(`test`, function () {
-  return gulp
-  .src([`js/**/*.test.js`])
-  .pipe(rollup({
-    plugins: [
-      commonjs()
-    ]}, `cjs`))
-  .pipe(gulp.dest(`build/test`))
-  .pipe(mocha({
-    reporter: `spec`
-  }));
-});
-
 gulp.task(`style`, () => {
   return gulp.src(`sass/style.scss`).
     pipe(plumber()).
@@ -128,5 +115,15 @@ gulp.task(`build`, [`assemble`], () => {
   gulp.start(`imagemin`);
 });
 
-gulp.task(`test`, () => {
+gulp.task(`test`, function () {
+  return gulp
+  .src([`js/**/*.test.js`])
+  .pipe(rollup({
+    plugins: [
+      commonjs()
+    ]}, `cjs`))
+  .pipe(gulp.dest(`build/test`))
+  .pipe(mocha({
+    reporter: `spec`
+  }));
 });
